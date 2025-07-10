788ddf82df6e1117c0f545fdb337a47d5f552216
ikh0120 <lkh50635164@gmail.com>
2025-07-10 20:29:12 +0900

    수정 사항 메모

--- src/main/java/com/alreadyemployee/alreadyemployee/chat/controller/ChatController.java
+++ src/main/java/com/alreadyemployee/alreadyemployee/chat/controller/ChatController.java
@@ -19,7 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/api/chat")
 @RequiredArgsConstructor
 public class ChatController {
-    private final LLMClient llmClient;
+
     private final RestClient restClient;
     private final ChatService chatService;
 
