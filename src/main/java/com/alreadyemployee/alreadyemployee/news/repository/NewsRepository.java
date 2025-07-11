1ffddef4e1ca46f1e66ccb4eb615bdf18b6b606f
ikh0120 <lkh50635164@gmail.com>
2025-07-11 19:11:01 +0900

    id랑 ìcontent s ë ts 반환

--- src/main/java/com/alreadyemployee/alreadyemployee/news/repository/NewsRepository.java
+++ src/main/java/com/alreadyemployee/alreadyemployee/news/repository/NewsRepository.java
@@ -1,6 +1,7 @@
 package com.alreadyemployee.alreadyemployee.news.repository;
 
 
+import com.alreadyemployee.alreadyemployee.news.controller.dto.NewsIdContentResponseDTO;
 import com.alreadyemployee.alreadyemployee.news.entity.News;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -18,4 +19,8 @@ public interface NewsRepository extends JpaRepository<News,Long> {
     // title만 가져오는 JPQL 쿼리
     @Query("SELECT n.title FROM News n WHERE n.companyName = :companyName")
     List<String> findTitlesByCompanyName(@Param("companyName") String companyName);
+
+    // 새로운 커스텀 쿼리: id와 contents만 선택
+    @Query("SELECT new com.alreadyemployee.alreadyemployee.news.controller.dto.NewsIdContentResponseDTO(n.id, n.contents) FROM News n")
+    List<NewsIdContentResponseDTO> findAllIdContent();
 }
