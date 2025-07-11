6ef82675a1aee3d18f1d97f7e9648445b14845b7
ikh0120 <lkh50635164@gmail.com>
2025-07-11 15:31:40 +0900

    publishDate ã추가

--- src/main/java/com/alreadyemployee/alreadyemployee/news/controller/dto/NewsDetailResponseDTO.java
+++ src/main/java/com/alreadyemployee/alreadyemployee/news/controller/dto/NewsDetailResponseDTO.java
@@ -2,9 +2,13 @@ package com.alreadyemployee.alreadyemployee.news.controller.dto;
 
 import lombok.*;
 
+import java.time.LocalDate;
+
 @Data @Builder
 @AllArgsConstructor
 public class NewsDetailResponseDTO {
     private String title;
     private String contents;
+    private LocalDate publishDate;
+
 }
