8783ed5630d6deb91f9090fdfb9977f84389e751
BitedRadish <a980831@naver.com>
2025-07-14 09:47:53 +0900

    fix : RefreshTokenService 수정

--- src/main/java/com/alreadyemployee/alreadyemployee/user/service/RefreshTokenService.java
+++ src/main/java/com/alreadyemployee/alreadyemployee/user/service/RefreshTokenService.java
@@ -1,9 +1,13 @@
 package com.alreadyemployee.alreadyemployee.user.service;
 
 
+import com.alreadyemployee.alreadyemployee.exception.BusinessException;
+import com.alreadyemployee.alreadyemployee.exception.ErrorCode;
 import com.alreadyemployee.alreadyemployee.user.entity.CustomUserDetails;
 import com.alreadyemployee.alreadyemployee.user.entity.RefreshToken;
+import com.alreadyemployee.alreadyemployee.user.entity.User;
 import com.alreadyemployee.alreadyemployee.user.repository.RefreshTokenRepository;
+import com.alreadyemployee.alreadyemployee.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -15,13 +19,17 @@ import java.time.LocalDateTime;
 public class RefreshTokenService {
 
     private final RefreshTokenRepository refreshTokenRepository;
+    private final UserRepository userRepository;
 
     // 저장 또는 갱신
     public void saveOrUpdate(Long userId, String refreshToken, long expiresInDays) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));;// 또는 userRepository.findById()
+
         RefreshToken token = refreshTokenRepository.findByUserId(userId)
                 .orElse(new RefreshToken());
 
-        token.setUserId(userId);
+        token.setUser(user);
         token.setRefreshToken(refreshToken);
         token.setCreatedAt(LocalDateTime.now());
         token.setExpiresAt(LocalDateTime.now().plusDays(expiresInDays));
